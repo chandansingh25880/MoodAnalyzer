49531cc43f9c03381ade30075bbd26ba914b1396
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-04-07 00:33:47 +0530

    Added Welcome to the MoodAnalyzer

---
+++ src/main/java/com/moodanalyzer/bridgelab/MoodAnalyAser.java
@@ -0,0 +1,5 @@
+package com.moodanalyzer.bridgelab;
+
+public class MoodAnalyAser {
+
+}
---
+++ src/test/java/com/moodanalyzer/bridgelab/MoodAnalyzerTest.java
@@ -0,0 +1,10 @@
+package com.moodanalyzer.bridgelab;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyzerTest {
+
+
+    
+}
